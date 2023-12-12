b5532e96abcf2410a8fe8eb6e109036397e710c9
1001shab <150432977+1001shab@users.noreply.github.com>
2023-12-12 17:29:33 +0330

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+using namespace std;
+int number1;
+int main()
+{
+    cout << "Please enter the number : \n";
+    cin >> number1;
+    cout << " The number is : " << number1;
+    //return 0 ;
+}
+
